8d506234a0b81bc54c388f7d7cbbf0a1760897b8
José Antonio <josejr017@gmail.com>
2025-03-17 16:34:13 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header"
+
+function App() {
+  return (
+    <div className="flex flex-col bg-gray-100">
+      <div className="pt-4 flex justify-center">
+        <span className="">Essa ferramenta tem como intuito ser utilizada para gerar instantaneamente uma senha aleatória e segura.</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
